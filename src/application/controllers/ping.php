1171e26d1a7a0a2e89664ce094075dab03098572
Nick Jackson <nijackson@lincoln.ac.uk>
2012-01-30 17:12:12 +0000

    Trying to get ping working

---
+++ src/application/controllers/ping.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Core extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function ping()
+	{
+	
+		$ping_response = $this->orbital->core_ping();
+	
+		$data = array(
+			'page_title' => 'Core Ping'
+			'ping_response_message' => $ping_response->message,
+			'ping_response_orbital_institution' => $ping_response->orbital->institution_name,
+			'ping_response_orbital_version' => $ping_response->orbital->core_version,
+			'ping_response_orbital_timestamp' => $ping_response->orbital->request_timestamp,
+		);
+	
+		$this->parser->parse('includes/header', $data);
+		$this->load->view('core_ping', $data);
+		$this->load->view('includes/footer');
+	}
+}
+
+// EOF
---
+++ src/application/libraries/Orbital.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+
+class Orbital {
+
+	private function get($target)
+	{
+	
+		try
+		{
+			return json_decode(file_get_contents($this->config->item('orbital_core_location') . $target));
+		}
+		catch (Exception $e)
+		{
+			return FALSE;
+		}
+	
+	}
+
+    public function core_ping()
+    {
+    
+    	return $this->get('core/ping');
+    
+    }
+}
+
+/* End of file Someclass.php */>
