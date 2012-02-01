c3ecda8b43fbbe5349ec2c2c942a3706087cd575
Nick Jackson <nijackson@lincoln.ac.uk>
2012-02-01 16:36:30 +0000

    Linky links!

--- src/application/views/includes/header.php
+++ src/application/views/includes/header.php
@@ -44,9 +44,9 @@
 					<a class="brand" href="#">{orbital_manager_name}</a>
 					<div class="nav-collapse">
 						<ul class="nav">
-							<li class="active"><a href="#">Home</a></li>
-							<li><a href="#about">About</a></li>
-							<li><a href="#contact">Contact</a></li>
+							<li class="active"><a href="{base_url}">Home</a></li>
+							<li><a href="{base_url}about">About</a></li>
+							<li><a href="{base_url}contact">Contact</a></li>
 						</ul>
 						<p class="navbar-text pull-right">Logged in as <a href="#">username</a></p>
 					</div>
