ed97babfe50d6f85f22e146c114c0d2903251d2c
Nick Jackson <nijackson@lincoln.ac.uk>
2012-02-07 16:16:22 +0000

    Nice error 500

---
+++ src/application/errors/error_500.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<title>Orbital Core</title>
+
+	<link href='https://fonts.googleapis.com/css?family=Spinnaker' rel='stylesheet' type='text/css'>
+
+	<style type="text/css">
+	
+		body {
+			background: #000 url(https://orbital.lncd.org/img/bg.png);
+			color: #FFF;
+			font-family: 'Spinnaker', sans-serif;
+			text-align: center;
+			font-size: 1.2em;
+			margin-top: 50px;
+		}
+		
+		a {
+			color: #F5D89A;
+		}
+	
+	</style>
+	
+</head>
+<body>
+
+	<h1><img src="https://orbital.lncd.org/img/logo.png" title="Orbital" alt="Orbital Logo"></h1>
+	
+	<h2>Whoops!</h2>
+	
+	<p>Something has gone quite badly wrong. Sorry about that.</p>
+
+</body>
+</html>
