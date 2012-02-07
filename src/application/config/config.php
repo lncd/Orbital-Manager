dd46a544adf4e852325fb780fccd6dfbde03d91d
Nick Jackson <nijackson@lincoln.ac.uk>
2012-02-07 15:04:45 +0000

    Database hookup for sessions

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -237,7 +237,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '##CI_ENCRYPTKEY##';
 
 /*
 |--------------------------------------------------------------------------
@@ -257,12 +257,12 @@ $config['encryption_key'] = '';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
+$config['sess_cookie_name']		= '##CI_NAME_PREFIX##_session';
 $config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= FALSE;
-$config['sess_encrypt_cookie']	= FALSE;
-$config['sess_use_database']	= FALSE;
-$config['sess_table_name']		= 'ci_sessions';
+$config['sess_expire_on_close']	= TRUE;
+$config['sess_encrypt_cookie']	= TRUE;
+$config['sess_use_database']	= TRUE;
+$config['sess_table_name']		= '##CI_SESSION_TABLE##';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
 $config['sess_time_to_update']	= 300;
@@ -277,7 +277,7 @@ $config['sess_time_to_update']	= 300;
 | 'cookie_path'   =  Typically will be a forward slash
 |
 */
-$config['cookie_prefix']	= '';
+$config['cookie_prefix']	= '##CI_NAME_PREFIX##_';
 $config['cookie_domain']	= '';
 $config['cookie_path']		= '/';
 
@@ -304,10 +304,10 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_cookie_name' = The cookie name
 | 'csrf_expire' = The number in seconds the token should expire.
 */
-$config['csrf_protection'] = FALSE;
-$config['csrf_token_name'] = 'csrf_test_name';
-$config['csrf_cookie_name'] = 'csrf_cookie_name';
-$config['csrf_expire'] = 7200;
+$config['csrf_protection'] = TRUE;
+$config['csrf_token_name'] = '##CI_NAME_PREFIX##_csrf_token';
+$config['csrf_cookie_name'] = '##CI_NAME_PREFIX##_csrf_cookie';
+$config['csrf_expire'] = 3600;
 
 /*
 |--------------------------------------------------------------------------
--- src/application/config/database.php
+++ src/application/config/database.php
@@ -41,11 +41,11 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['hostname'] = '##MYSQL_HOSTNAME##';
+$db['default']['username'] = '##MYSQL_USERNAME#';
+$db['default']['password'] = '##MYSQL_PASSWORD##';
+$db['default']['database'] = '##MYSQL_DATABASE##';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
@@ -55,7 +55,7 @@ $db['default']['char_set'] = 'utf8';
 $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
+$db['default']['stricton'] = TRUE;
 
 
 /* End of file database.php */
