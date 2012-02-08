a838ab585d68fc527e082fb4d5f764dbeb5d6419
Nick Jackson <nick@nickjackson.me>
2012-02-08 08:56:30 +0000

    New about blurbs

--- src/application/views/static/about.php
+++ src/application/views/static/about.php
@@ -1,29 +1,29 @@
 <div class="hero-unit">
   <h1>Managing Research Data. Easy.</h1>
-  <p>{orbital_manager_name} is a service to help you manage your research data, keep track of what you've got, publish data, perform lighting-fast analysis and more.</p>
+  <p>Orbital is a service to help you manage your research data, keep track of what you've got, publish data, perform lighting-fast analysis and more.</p>
   <p>Find out what it's got to offer.</p>
 </div>
 
 <div class="row-fluid">
 	<div class="span4">
 		
-		<h2>Safe and secure.</h2>
+		<h2>With you all the way.</h2>
 		
-		<p>Orbital...</p>
-		
-	</div>
-	<div class="span4">
-		
-		<h2>Safe and secure.</h2>
-		
-		<p>Orbital...</p>
+		<p>From bid to publication, Orbital helps you plan, store, manage and retrieve your research data, making meeting those funding body criteria easier than ever.</p>
 		
 	</div>
 	<div class="span4">
 		
-		<h2>Safe and secure.</h2>
+		<h2>At your fingertips.</h2>
 		
-		<p>Orbital...</p>
+		<p>Orbital can keep track of hundreds, thousands or even millions of bits of research data, so it can get what you're looking for in front of you in seconds.</p>
+		
+	</div>
+	<div class="span4">
+		
+		<h2>Not just a place for files.</h2>
+		
+		<p>Where before you would just store files, Orbital goes a step further and takes a look inside to extract as much of the data as it can into a smart, self-organising database. Instantly turn a pile of Excel spreadsheets into indexed data, run full-text searches on documents</p>
 		
 	</div>
 </div>
@@ -33,9 +33,9 @@
 <div class="row-fluid">
 	<div class="span4">
 		
-		<h2>Safe and secure.</h2>
+		<h2>Data safety done right.</h2>
 		
-		<p>Orbital...</p>
+		<p>Losing research data can be catastrophic for a project, which is why Orbital keeps your data not just on a hard disk somewhere, but spread across redundant systems in multiple locations.</p>
 		
 	</div>
 	<div class="span4">
