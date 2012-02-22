835b9a74cdc52cfd841e1857bc00ffdd7525ec70
Nick Jackson <nick@nickjackson.me>
2012-02-22 21:30:02 +0000

    Actually remembered to save a change this time.

--- src/application/views/includes/header.php
+++ src/application/views/includes/header.php
@@ -42,6 +42,9 @@
 					<a class="brand" href="#">{orbital_manager_name}</a>
 					<div class="nav-collapse">
 						<ul class="nav">
+							{nav_menu}
+								<li><a href="{uri}">{name}</a></li>
+							{/nav_menu}
 							<li><a href="{base_url}">Home</a></li>
 							<li><a href="{base_url}about">About</a></li>
 							<li><a href="{base_url}contact">Contact</a></li>
