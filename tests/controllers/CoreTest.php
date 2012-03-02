ea7ec39f1feea813699ea192d0efc38de9e3ba73
Nick Jackson <nijackson@lincoln.ac.uk>
2012-03-02 12:13:15 +0000

    Tests

--- tests/controllers/CoreTest.php
+++ tests/controllers/CoreTest.php
@@ -9,6 +9,12 @@ class CoreTest extends CIUnit_TestCase
 	public function setUp()
 	{
 		// Set the tested controller
-		// $this->CI = set_controller('core');
+		$this->CI = set_controller('core');
+	}
+	
+	public function testPing()
+	{
+		// Call the controllers method
+		$this->CI->ping();
 	}
 }
