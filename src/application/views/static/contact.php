c1f7f7c1a76ce93b8859ab916f51566359544ead
Nick Jackson <nijackson@lincoln.ac.uk>
2012-04-30 16:45:44 +0100

    Nicer contacts!

--- src/application/views/static/contact.php
+++ src/application/views/static/contact.php
@@ -20,14 +20,63 @@
 <div class="row">
 	<div class="span8">
 	
+		<p>The Orbital project team are here if you want to get in touch with us about anything. If you've got a question about the project, email our Project Manager. If you've got a development question, email one of our development team.</p>
 		
-		<p>We love hearing from people, so we've made it easy. Just drop us an email to <b><a href="mailto:orbital@lncd.org">orbital@lncd.org</a></b>, and let us know what you're thinking about.</p>
+		<h2>Meet The Team</h2>
 		
-		<p>If Twitter is more your thing you can either mention our hashtag <b>#OrbitalMRD</b>, or tweet directly at the lead developer <b><a href="http://twitter.com/jacksonj04">@jacksonj04</a></b></p>
+		<div class="row">
+			<div class="span4">
+				<img src="http://www.gravatar.com/avatar/4270169f7652a37e9d18e147d3c87c0f?s=80&d=retro" class="pull-left" style="margin:6px;">
+				<h3>Joss Winn</h3>
+				<p>Project Manager<br>
+				<a href="mailto:jwinn@lincoln.ac.uk">jwinn@lincoln.ac.uk</a></p>
+			</div>
+			<div class="span4">
+				<img src="http://www.gravatar.com/avatar/ff08ce386584520ef8bc6b271f978d68?s=80&d=retro" class="pull-left" style="margin:6px;">
+				<h3>Nick Jackson</h3>
+				<p>Lead Developer<br>
+				<a href="mailto:nijackson@lincoln.ac.uk">nijackson@lincoln.ac.uk</a></p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="span4">
+				<img src="http://www.gravatar.com/avatar/a036ec8ffc4bc25e071c0d75c5901f93?s=80&d=retro" class="pull-left" style="margin:6px;">
+				<h3>Harry Newton</h3>
+				<p>Developer<br>
+				<a href="mailto:hnewton@lincoln.ac.uk">hnewton@lincoln.ac.uk</a></p>
+			</div>
+			<div class="span4">
+				<img src="http://www.gravatar.com/avatar/d21720f93116c4b5063e29ccfff016f8?s=80&d=retro" class="pull-left" style="margin:6px;">
+				<h3>Paul Stainthorp</h3>
+				<p>Lead Researcher</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="span4">
+				<img src="http://www.gravatar.com/avatar/9322b46777cf6af23e78d8e54f1e806d?s=80&d=retro" class="pull-left" style="margin:6px;">
+				<h3>Bev Jones</h3>
+				<p>Researcher</p>
+			</div>
+			<div class="span4">
+				<img src="http://www.gravatar.com/avatar/6c3c231a7fbdab2b4deaae50628b0267?s=80&d=retro" class="pull-left" style="margin:6px;">
+				<h3>Annalisa Jones</h3>
+				<p>Researcher</p>
+			</div>
+		</div>
 		
 	</div>
 	
 	<div class="span4">
+	
+		<div class="well">
+	
+			<h2>Twitter</h2>
+			
+			<p>If Twitter is more your thing you can either mention our hashtag <b>#OrbitalMRD</b>, or tweet directly at the lead developer <b><a href="http://twitter.com/jacksonj04">@jacksonj04</a></b></p>
+		
+		</div>
 	
 		<div class="well">
 	
