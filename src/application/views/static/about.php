ee6a3c44651f24e232e6f69381cfd8d87a6703a0
Nick Jackson <nijackson@lincoln.ac.uk>
2012-04-30 16:05:34 +0100

    About page is now back to the trio

--- src/application/views/static/about.php
+++ src/application/views/static/about.php
@@ -11,4 +11,25 @@
 	<h1><?php echo lang('about_title'); ?></h1>
 </div>
 
-<?php echo auto_typography(lang('about_text')); ?>
+<?php echo auto_typography(lang('about_main_text')); ?>
+
+<hr>
+
+<div class="row">
+
+	<div class="span4">
+		<h2><?php echo lang('about_trio1_title'); ?></h2>
+		<?php echo auto_typography(lang('about_trio1_text')); ?>
+	</div>
+	
+	<div class="span4">
+		<h2><?php echo lang('about_trio2_title'); ?></h2>
+		<?php echo auto_typography(lang('about_trio2_text')); ?>
+	</div>
+	
+	<div class="span4">
+		<h2><?php echo lang('about_trio3_title'); ?></h2>
+		<?php echo auto_typography(lang('about_trio3_text')); ?>
+	</div>
+
+</div>
