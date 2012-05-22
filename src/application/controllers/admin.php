ddde05a5a2fd079f521f28afa625402421361889
Harry Newton <h-newton@hotmail.com>
2012-05-22 12:28:29 +0100

    Removed cost calculator from project page
    And added page title for Project Planner page

--- src/application/controllers/admin.php
+++ src/application/controllers/admin.php
@@ -235,6 +235,7 @@ class Admin extends CI_Controller {
 	
 	function project_planner()
 	{	
+		$this->data['page_title'] = 'Project Planner';
 		$this->parser->parse('includes/header', $this->data);
 		$this->parser->parse('static/project_planner', $this->data);
 		$this->parser->parse('includes/footer', $this->data);
